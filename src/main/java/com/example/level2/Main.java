32b4ffd27cf0c28332e41e4bd52fa0a0831d8881
abc <clvlajrrhtlvek@gmail.com>
2025-01-15 16:28:01 +0900

    Refactor : "주석 추가 및 변수명 변경"
    다른 사용자가 알아보기 쉽도록 주석을 추가하고 변수명을 변경하였다.

--- src/main/java/com/example/level2/Main.java
+++ src/main/java/com/example/level2/Main.java
@@ -6,37 +6,37 @@ import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
-        List<MenuItem> menuItems = new ArrayList<>();
+        List<MenuItem> burgers = new ArrayList<>();
 
-        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소그가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소그가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+        //버거 종류를 배열에 추가하기
+        burgers.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소그가 토핑된 치즈버거"));
+        burgers.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소그가 토핑된 치즈버거"));
+        burgers.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        burgers.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
 
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            //반복문을 활용해 메뉴를 하나씩 출력한다.
+            //메뉴 출력
             System.out.println("[ SHAKESHACK MENU ]");
-            for (int i = 0; i < menuItems.size(); i++) {
-                MenuItem item = menuItems.get(i);
+            for (int i = 0; i < burgers.size(); i++) {
+                MenuItem item = burgers.get(i);
                 System.out.println((i + 1) + ". " + item.getName() + " |W" + item.getPrice() + "| " + item.getManual());
             }
             System.out.println("0. 종료 | 종료");
 
-            //입력 받은 숫자가 0 -> 프로그램 종료
-            //입력 받은 숫자가 메뉴에 있는 경우 -> 선택한 메뉴 정보 출력
-            //입력 받은 값이 숫자가 아닌경우, 입력 받은 숫자가 메뉴에 없는 경우 -> 다시 입력
+            //입력에 따른 예외 처리
             try {
                 int choose = sc.nextInt();
+
                 if (choose == 0) {
                     System.out.println("프로그램을 종료합니다.");
                     break;
                 } else if (choose > 4) {
                     System.out.println("해당 메뉴는 존재하지 않습니다. 다시 선택해주세요.\n");
                 } else {
-                    MenuItem chooseItem = menuItems.get(choose -1);
+                    MenuItem chooseItem = burgers.get(choose -1);
                     System.out.println("선택한 메뉴: " + chooseItem.getName() + ", " + chooseItem.getPrice() + ", " + chooseItem.getManual());
                 }
             } catch (InputMismatchException e) {
