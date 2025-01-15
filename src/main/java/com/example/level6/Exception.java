2e927d6d518aa1fcd493d2b59541cb9fea20a480
abc <clvlajrrhtlvek@gmail.com>
2025-01-15 21:43:01 +0900

    Refactor : "예외처리 클래스 생성"
    예외처리 클래스를 새로 만들어 책임을 분활 시켰다.

---
+++ src/main/java/com/example/level6/Exception.java
@@ -0,0 +1,12 @@
+package com.example.level6;
+
+import java.util.Scanner;
+
+public class Exception {
+    private int choose;
+    private final Scanner sc = new Scanner(System.in);
+
+    public void InputException() {
+
+    }
+}
